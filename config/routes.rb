fc7fe1ef789b47ba49a5be52834d383f2bdfb9bb
michaeljxzhu <michael.jx.zhu@gmail.com>
2016-03-12 06:31:33 +0000

    root redirects to /admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  #get 'welcome/index' , to: '/admin'
   
   # audition_form_controller
   get '/auditionform', to: 'audition_form#index'
