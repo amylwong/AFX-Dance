bfe4b9f45365c451293efd36a2d9fdf43303cf55
michaeljxzhu <michael.jx.zhu@gmail.com>
2016-04-01 10:49:21 +0000

    created initial views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+#AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 Dancer.create!(name: 'Michael', audition: 2, email: 'test@example.com', phone: '909-569-5555', year: '1', gender: 'M')
