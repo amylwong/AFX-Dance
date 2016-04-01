60ea9e3ffa2371346de3185bdd579995227d58b3
michaeljxzhu <michael.jx.zhu@gmail.com>
2016-04-01 12:45:57 +0000

    added url validator for casting_groups and hyperlink to video

--- app/admin/casting_group.rb
+++ app/admin/casting_group.rb
@@ -1,5 +1,19 @@
 ActiveAdmin.register CastingGroup do
+    
     permit_params :video
+    
+    show do
+        
+        casting_group = CastingGroup.find(params[:id])
+        
+        attributes_table *default_attribute_table_rows
+        
+        panel "Audition Video" do
+            text_node link_to(casting_group.video, casting_group.video, method: :get).html_safe
+        end
+        
+        active_admin_comments
+    end
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
