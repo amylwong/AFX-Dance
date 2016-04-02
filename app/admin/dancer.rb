9bbcf1c0e328640e91a42db238315c66367e0139
albertweng13 <albertweng@berkeley.edu>
2016-04-02 02:01:20 +0000

    hi

--- app/admin/dancer.rb
+++ app/admin/dancer.rb
@@ -15,6 +15,9 @@ ActiveAdmin.register Dancer do
 #   permitted
 # end
 
+    #TODO!!!!
+    currTeam = Team.find(1) #Replace this with dynamically obtained team (its hardcoded right now as team 1)
+    
     batch_action :add_to_my_team do |ids|
         #Dancer.find(ids).each do |dancer|
             #flash[:notice] = "Hi #{dancer}"
@@ -25,8 +28,8 @@ ActiveAdmin.register Dancer do
         add = false
         Dancer.find(ids).each do |id|
         #    flash[:notice] = "Hi #{id}"
-            if not id.teams.include? Team.find(1)
-                id.teams << Team.find(1)
+            if not id.teams.include? currTeam
+                id.teams << currTeam
                 id.save
                 add = true
             end
@@ -48,9 +51,9 @@ ActiveAdmin.register Dancer do
         list = []
         delete = false
         Dancer.find(ids).each do |id|
-            if id.teams.include? Team.find(1)
-                Team.find(1).dancers.delete(id)
-                Team.find(1).save
+            if id.teams.include? currTeam
+                currTeam.dancers.delete(id)
+                currTeam.save
                 delete = true
                 list << id.name
             end
