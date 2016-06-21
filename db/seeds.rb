52a0eec9453de874aad9ee39c1a001081fdea1dd
michaeljxzhu <michael.jx.zhu@gmail.com>
2016-06-21 02:36:33 +0000

    remove people from seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-AdminUser.create(email: 'admin@example.com', password: 'password', password_confirmation: 'password', admin_type: 'admin', names: 'Albert, Calvin')
-AdminUser.create(email: 'board@example.com', password: 'password', password_confirmation: 'password', admin_type: 'board', names: 'Albert, Michael')
-AdminUser.create(email: 'project@example.com', password: 'password', password_confirmation: 'password', admin_type: 'project', names: 'Albert, Angela')
-AdminUser.create(email: 'training@example.com', password: 'password', password_confirmation: 'password', admin_type: 'training', names: 'Albert, Alice')
-Dancer.create(name: 'Michael', email: 'michael@example.com', phone: '909-569-5555', year: '1', gender: 'M')
+# AdminUser.create(email: 'afx@afx.com', password: 'password', password_confirmation: 'password', admin_type: 'admin', names: 'Albert, Calvin')
