4daae0fafeac1ac27cd16a966eb726302433a161
michaeljxzhu <michael.jx.zhu@gmail.com>
2016-08-27 01:59:53 +0000

    Remove unused files, reset migrations

--- db/migrate/20160415214356_add_names_to_admin_users.rb
+++
@@ -1,5 +0,0 @@
-class AddNamesToAdminUsers < ActiveRecord::Migration
-  def change
-    add_column :admin_users, :names, :string
-  end
-end
--- db/migrate/20160429114207_add_available_to_dancer.rb
+++
@@ -1,5 +0,0 @@
-class AddAvailableToDancer < ActiveRecord::Migration
-  def change
-    add_column :dancers, :available, :boolean, default: true
-  end
-end
